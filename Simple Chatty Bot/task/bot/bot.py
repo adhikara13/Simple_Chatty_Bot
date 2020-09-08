7db87004f8a8b8dfbfcc37976916868b8c997ae0
adhikara13 <adhikara13@gmail.com>
2020-09-08 03:14:19 +0300

    Finished the stage 2.

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -3,5 +3,5 @@ print('I was created in 2020.')
 print('Please, remind me your name.')
 
 # reading a name
-
-print('What a great name you have, {your_name}!')
+your_name = input()
+print('What a great name you have, !' + your_name)
